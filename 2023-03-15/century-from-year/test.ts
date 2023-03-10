06284d528e4586a75cb288f695f206d606ed4c10
Simon Flack <simonflack@gmail.com>
2023-03-10 18:03:47 +0100

    Fix century from year tests

--- 2023-03-15/century-from-year/test.ts
+++ 2023-03-15/century-from-year/test.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect } from 'vitest';
 
 export function makeTest(
-  solution: (n: number, s: string) => string,
+  solution: (year: number) => number,
   enabled = true,
 ) {
   if (!enabled) {
@@ -10,10 +10,34 @@ export function makeTest(
     });
     return;
   }
-
-  describe('Tests', () => {
-    it('test', () => {
-      expect(solution(3, '*')).toEqual('***');
-    });
+  
+  describe("solution", () => {
+      it("should return 18 when input is 1705", () => {
+          const expected = 18;
+          const actual = solution(1705);
+  
+          expect(actual).to.eql(expected);
+      });
+  
+      it("should return 19 when input is 1900", () => {
+          const expected = 19;
+          const actual = solution(1900);
+  
+          expect(actual).to.eql(expected);
+      });
+  
+      it("should return 17 when input is 1601", () => {
+          const expected = 17;
+          const actual = solution(1601);
+  
+          expect(actual).to.eql(expected);
+      });
+  
+      it("should return 20 when input is 2000", () => {
+          const expected = 20;
+          const actual = solution(2000);
+  
+          expect(actual).to.eql(expected);
+      });
   });
-}
+  }
