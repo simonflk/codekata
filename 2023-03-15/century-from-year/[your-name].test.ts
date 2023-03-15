288820ad8030b780f8d742044eb02566e66c7937
kevingorjanc <48233354+kevingorjanc@users.noreply.github.com>
2023-03-15 08:08:00 -0700

    KG 3-15-2023 (#4)
    Co-authored-by: kevin gorjanc <kevin@tutorolearning.com>

--- 2023-03-15/century-from-year/[your-name].test.ts
+++
@@ -1,9 +0,0 @@
-import { makeTest } from './test';
-
-//? Set to `true` when you are ready to enable tests
-const RUN_TESTS = false;
-makeTest(centuryFromYear, RUN_TESTS);
-
-export function centuryFromYear(year: number): number {
-  return 1;
-}
--- 2023-03-15/string-repeat/kg.test.ts
+++ 2023-03-15/string-repeat/kg.test.ts
@@ -1,9 +1,10 @@
 import { makeTest } from './test';
 
 //? Set to `true` when you are ready to enable tests
-const RUN_TESTS = false;
+const RUN_TESTS = true;
 makeTest(repeatStr, RUN_TESTS);
 
 export function repeatStr(n: number, s: string) {
-  return '';
+  
+  return (s.repeat(n))
 }
