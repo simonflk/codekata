9685f03d66b66079046504d2cd34d3ee5afcae85
Simon Flack <simonflack@gmail.com>
2023-03-18 17:22:37 +0100

    Restore template files week 2

---
+++ 2023-03-15/lets-play-darts/[your-name].test.js
@@ -0,0 +1,10 @@
+import { makeTest } from './test';
+
+//? Set to `true` when you are ready to enable tests
+const RUN_TESTS = false;
+makeTest(getDartboardScore, RUN_TESTS);
+
+function getDartboardScore(x, y) {
+  // Start your coding here...
+  return '';
+}
