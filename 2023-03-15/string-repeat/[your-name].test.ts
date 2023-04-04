0c0d8747b7fbf073c17d82d004dd8eec52c231af
Simon Flack <simonflack@gmail.com>
2023-04-04 19:39:27 +0200

    Restore the missing templates

---
+++ 2023-03-15/string-repeat/[your-name].test.ts
@@ -0,0 +1,9 @@
+import { makeTest } from './test';
+
+//? Set to `true` when you are ready to enable tests
+const RUN_TESTS = false;
+makeTest(repeatStr, RUN_TESTS);
+
+export function repeatStr(n: number, s: string) {
+  return '';
+}
---
+++ 2023-03-29/credit-card-mask/[your-name].test.js
@@ -0,0 +1,14 @@
+// @ts-check
+import { makeTest } from './test';
+
+//? Set to `true` when you are ready to enable tests
+const RUN_TESTS = false;
+makeTest(maskify, RUN_TESTS);
+
+/**
+ * Return the masked string
+ * @param {string} cc
+ */
+function maskify(cc) {
+  return cc;
+}
