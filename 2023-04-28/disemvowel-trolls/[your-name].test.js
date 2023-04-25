4864f4ca4948da71b39158641f066dd995d8103f
Simon Flack <simonflack@gmail.com>
2023-04-25 18:04:22 +0200

    📝 Week 9 templates

---
+++ 2023-04-28/disemvowel-trolls/[your-name].test.js
@@ -0,0 +1,13 @@
+// @ts-check
+import { makeTest } from './test';
+
+//? Set to `true` when you are ready to enable tests
+const RUN_TESTS = false;
+makeTest(disemvowel, RUN_TESTS);
+
+/**
+ * @param {string} str 
+ */
+function disemvowel(str) {
+  return str;
+}
---
+++ 2023-04-28/flatten-sort-array/[your-name].test.js
@@ -0,0 +1,15 @@
+// @ts-check
+import { makeTest } from './test';
+
+//? Set to `true` when you are ready to enable tests
+const RUN_TESTS = false;
+makeTest(flattenAndSort, RUN_TESTS);
+
+/**
+ * @param {Array<number[]>} array 
+ * @returns {number[]}
+ */
+function flattenAndSort(array) {
+  // Good luck, brave code warrior!
+  return array;
+}
