778e1b40b0b5d3bd8b898c658344bf2a48bf5d89
Simon Flack <simonflack@gmail.com>
2023-08-13 21:22:14 +0200

    SF: Final total-inc-dec solution

--- 2023-08-11/total-inc-dec/simon.test.js
+++ 2023-08-11/total-inc-dec/simon.test.js
@@ -5,58 +5,57 @@ import { makeTest } from './test';
 const RUN_TESTS = true;
 makeTest(totalIncDec, RUN_TESTS);
 
-/**
- * @typedef {Set<number>} IncDecNumbers
- * @typedef {Map<number, IncDecNumbers>} PowersOfTenCache
- */
-
-/** @type {PowersOfTenCache} */
-const cache = new Map([
-  [0, new Set([0])],
-  [1, new Set(range(1, 9))],
-]);
-
-function range(start = 0, end = 9, step = 1) {
-  if (start > end) step = -step;
-  const length = Math.abs(end - start) + 1;
-  return [...Array(length)].map((_, i) => i * step + start);
-}
+// For each power of 10 keep track of the number of
+// increasing and decreasing numbers that *end* with
+// each digit. The index of the array is the power of 10
+const increasing = [
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
+const decreasing = structuredClone(increasing);
+const allDigits = [0,1,2,3,4,5,6,7,8,9]
 
 /**
  * @param {number} x
  */
 function totalIncDec(x) {
   let total = 0;
+  const sum = (/**@type {number[]} */ arr) => arr.reduce((a, b) => a + b, 0);
   while (x >= 0) {
-    total += getNumbersForPower(x).size || 0;
+    const [inc, dec] = getNumbersForPower(x);
+    const overlapCount = x ? 9 : 1; // number of items in both lists
+    total += sum(inc) + sum(dec) - overlapCount;
     x--;
   }
 
   return total;
 
-  function getNumbersForPower(/** @type {number} */ n) {
-    /** @type {IncDecNumbers} */
-    const thisPower = cache.get(n) ?? new Set();
-    if (thisPower.size) return thisPower;
+  /**
+   * @param {number} n
+   * @returns [number[], number[]]
+   */
+  function getNumbersForPower(n) {
+    const thisInc = increasing[n];
+    const thisDec = decreasing[n];
+    if (thisInc && thisDec) return [thisInc, thisDec];
 
-    // We need to calculate this power
-    const previousPower = getNumbersForPower(n - 1);
-    for (const num of [...previousPower]) {
-      const firstDigit = Math.floor(num / 10 ** (n - 2));
-      const lastDigit = num % 10;
+    const [prevInc, prevDec] = getNumbersForPower(n - 1);
+    const inc = allDigits.map((digit) => {
+      // sum of all previous power's ending digits up to and
+      // including this one
+      return sum(prevInc.slice(0, digit + 1));
+    });
+    const dec = allDigits.map((digit) => {
+      // Sum of all previous power's ending digits starting
+      // with this one
+      return sum(prevDec.slice(digit));
+    });
 
-      const numbersToAdd =
-        firstDigit === lastDigit
-          ? range()
-          : firstDigit > lastDigit
-          ? range(lastDigit, 0)
-          : range(lastDigit);
-      numbersToAdd.forEach((digit) => {
-        thisPower.add(num * 10 + digit);
-      });
-    }
+    increasing[n] = inc;
+    decreasing[n] = dec;
 
-    cache.set(n, thisPower);
+    /** @type {[number[], number[]]} */
+    const thisPower = [inc, dec];
     return thisPower;
   }
 }
